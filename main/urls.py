2dd5582085559264ab38c1e0b807490c1baf045a
abbasguliyev <abbasquliyev111@gmail.com>
2022-09-21 15:35:06 +0400

    test

--- main/urls.py
+++ main/urls.py
@@ -14,13 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.v1.urls'))
+    path('api/v1/', include('api.v1.urls')),
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ]
 
 if settings.DEBUG:
