6c961da8a8f5e286252b0a54eafd0dc007920bf7
abbasguliyev <abbasquliyev111@gmail.com>
2022-10-20 20:26:27 +0400

    added media folder

--- main/settings.py
+++ main/settings.py
@@ -209,9 +209,10 @@ CORS_ALLOW_HEADERS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
+# STATIC_URL = 'static/'
+# STATIC_ROOT = 'static/'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR)
