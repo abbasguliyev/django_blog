1e8c1803b263d9373072a731365a1b422f35358c
abbasguliyev <abbasquliyev111@gmail.com>
2022-10-20 20:29:17 +0400

    added media folder

--- main/settings.py
+++ main/settings.py
@@ -211,8 +211,7 @@ CORS_ALLOW_HEADERS = [
 
 # STATIC_URL = 'static/'
 # STATIC_ROOT = 'static/'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR)
