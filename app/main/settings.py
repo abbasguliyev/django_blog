e130a038c04c3535f71e57d602f9a19ff17bfbcc
abbasguliyev <abbasquliyev111@gmail.com>
2022-12-06 08:02:18 +0400

    fixed some bugs

--- app/main/settings.py
+++ app/main/settings.py
@@ -16,15 +16,11 @@ import os
 from django.conf import settings
 from pathlib import Path
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
