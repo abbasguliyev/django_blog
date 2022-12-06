de1899ef14fecccd9450f48522ff75a8ef6bc12c
abbasguliyev <abbasquliyev111@gmail.com>
2022-12-06 13:23:22 +0400

    fixed some bugs

--- app/main/wsgi.py
+++ app/main/wsgi.py
@@ -8,11 +8,8 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root="/static")
-application.add_files("/static", prefix="/")
