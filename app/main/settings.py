143a05bd1f22552ebe9a99f33117ca9fad33b5e4
abbasguliyev <abbasquliyev111@gmail.com>
2022-12-08 18:02:40 +0400

    fixed some bugs

--- app/main/settings.py
+++ app/main/settings.py
@@ -34,7 +34,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['api.vilayatsafarov.com','37.26.63.164']
 
-CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = False
 # SESSION_COOKIE_SECURE = False
 ALLOWED_ORIGINS = ['http://*', 'https://*']
 CSRF_TRUSTED_ORIGINS = ['https://api.vilayatsafarov.com/']
