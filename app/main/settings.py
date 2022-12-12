70537f36959590c75c00d2cff388130cba7e7e08
abbasguliyev <abbasquliyev111@gmail.com>
2022-12-12 10:26:16 +0400

    fixed some bugs

--- app/main/settings.py
+++ app/main/settings.py
@@ -122,7 +122,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 12,
 }
 
 
