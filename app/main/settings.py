93f85a256c2a61a10e60b30a9a0613a5c120b6dc
abbasguliyev <abbasquliyev111@gmail.com>
2023-02-18 20:02:58 +0400

    fixed bugs

--- app/main/settings.py
+++ app/main/settings.py
@@ -163,7 +163,7 @@ DATABASES = {
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_HOST'],
+        'HOST': "db",
         'PORT': os.environ['DB_PORT'],
     }
 }
